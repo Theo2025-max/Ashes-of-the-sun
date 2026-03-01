eba5e0c359485108f0e51dda74bd2d864039be60
Theo2025-max <madzunyetheo@gmail.com>
2026-03-01 09:13:56 +0200

    [2026/03/01]
    -WIP:LEVEL DESIGN IN PROGRESS

--- Ashes of the sun/Assets/Scripts/Player/DamageTrigger.cs
+++ Ashes of the sun/Assets/Scripts/Player/DamageTrigger.cs
@@ -5,10 +5,16 @@ public class DamageTrigger : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+        PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>(); 
 
         if (playerController != null)
         {
-            playerController.knockback();
+            playerController.knockback(transform.position.x);
+        }
+
+        if (playerHealth != null) 
+        {
+            playerHealth.TakeDamage(1);
         }
     }
 }
